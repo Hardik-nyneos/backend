79b62579d88592f506a0891312cf9addae1be214
Hardik-nyneos <hardik.mishra@nyneos.com>
2025-07-31 03:33:18 +0530

    Update exposureBucketing.js

--- routes/exposureBucketing.js
+++ routes/exposureBucketing.js
@@ -3,13 +3,25 @@ const router = express.Router();
 
 const exposureBucketingController = require("../controllers/exposureBucketingController");
 
-
 router.get("/userVars", exposureBucketingController.getUserVars);
 router.get("/renderVars", exposureBucketingController.getRenderVars);
 // router.get("/pendingrenderVars", exposureUploadController.getPendingApprovalVars);
 router.get("/userJourney", exposureBucketingController.getUserJourney);
 router.post("/bulkApprove", exposureBucketingController.approveBucketing);
 router.post("/bulkReject", exposureBucketingController.rejectMultipleExposures);
+router.get(
+  "/joined-exposures",
+  exposureBucketingController.getExposureHeadersLineItemsBucketing
+);
+// Approve/reject exposure_bucketing status (status only, no delete logic)
+router.post("/approve", exposureBucketingController.approveBucketingStatus);
+router.post("/reject", exposureBucketingController.rejectBucketingStatus);
+
+// Update joined exposures (header, line item, bucketing)
+router.post(
+  "/:exposure_header_id/update",
+  exposureBucketingController.updateExposureHeadersLineItemsBucketing
+);
 router.post("/:id/edit", exposureBucketingController.getupdate);
 
 module.exports = router;
