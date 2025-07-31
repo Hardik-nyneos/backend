6ef747853ee61a70375cfa433d6a7e2677bc99a7
Kanav Arora <125301399+Kanav-1822@users.noreply.github.com>
2025-07-31 05:33:51 +0530

    Update forwardBookings.js

--- routes/forwardBookings.js
+++ routes/forwardBookings.js
@@ -1,3 +1,5 @@
+// Link exposure to hedge booking
+
 // Manual entry for forward confirmations
 
 const express = require('express');
@@ -6,7 +8,9 @@ const forwardController = require('../controllers/forwardController');
 
 // Manual entry for forward bookings
 router.post('/forward-bookings/manual-entry', forwardController.addForwardBookingManualEntry);
-
+// Get all forward_bookings relevant to user's accessible entities
+router.get('/forward-bookings/forwardDetails', forwardController.getEntityRelevantForwardBookings);
+router.post('/exposure-hedge-links/link', forwardController.linkExposureHedge);
 // Multi-file upload for forward bookings (CSV/Excel)
 router.post('/forward-bookings/upload-multi', forwardController.upload.array('files'), forwardController.uploadForwardBookingsMulti);
 router.post('/forward-confirmations/manual-entry', forwardController.addForwardConfirmationManualEntry);
